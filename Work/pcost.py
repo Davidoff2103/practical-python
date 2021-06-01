05e3689405f16714dee7d274674366de6cdc880f
Davidoff2103 <davidoff.d777@gmail.com>
2021-06-01 10:14:25 +0200

    exercici suma cost

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,3 +1,15 @@
 # pcost.py
-#
-# Exercise 1.27
+
+suma = 0
+
+f = open('Data/portfolio.csv', 'rt')
+headers = next(f)
+for line in f:
+    row = line.split(',')
+    shares = int(row[1])
+    purchase = float(row[2])
+    suma = suma + shares * purchase
+
+f.close()
+
+print(f'The cost to purchase all of the shares in the portfolio is {suma}€')
