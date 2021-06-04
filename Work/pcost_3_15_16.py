40122cc0093cd7ca8b3f6ea4bfbc9655666ed5fd
Davidoff2103 <davidoff.d777@gmail.com>
2021-06-04 12:51:43 +0200

    exercicis funcions executables com a script

--- Work/pcost_3_15_16.py
+++ Work/pcost_3_15_16.py
@@ -12,3 +12,7 @@ def main(args):
         raise SystemExit('Usage: %s portfoliofile' % args[0])
     filename = args[1]
     print('Total cost:', portfolio_cost(filename))
+
+if __name__ == '__main__':
+    import sys
+    main(sys.argv)
